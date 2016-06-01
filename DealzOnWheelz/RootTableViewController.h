c90e63b8d4e091dbfd4064faf4516a27e06f4d58
Brandon Manson <brandon.manson@detroitlabs.com>
2016-06-01 10:01:58 -0400

    added navigation controller and UITableViewController subclass

---
+++ DealzOnWheelz/RootTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootTableViewController.h
+//  DealzOnWheelz
+//
+//  Created by Brandon Manson on 6/1/16.
+//  Copyright © 2016 Detroit Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootTableViewController : UITableViewController
+
+@end
